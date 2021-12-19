cfea509024e7dcdf505ca62fbb4c9964f6ffa850
Tamojit Roy <astrodestroyer146@gmail.com>
2021-12-19 22:02:50 +0530

    Added Session 10 and 11

---
+++ SESSION #10/CODE.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+	int data;
+	int power;
+	struct Node * next;
+};
+
+struct Node * start1 = (struct Node *)malloc(sizeof(struct Node *));
+struct Node * start2 = (struct Node *)malloc(sizeof(struct Node *));
+
+// Same length lists
+void sumof2polynomials(struct Node * h1, struct Node * h2)
+{
+	
+}
+
+// Different length lists
+void sumof2polynomials(struct Node * h1, struct Node * h2)
+{
+	
+}
+
+int main()
+{
+	
+	
+	return 0;
+}
+
+
